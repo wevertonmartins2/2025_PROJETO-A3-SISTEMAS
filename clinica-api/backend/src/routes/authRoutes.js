4ec7d4d87d388530a0b8f9152f5596b653494c9b
FlavioGrego <322129707@ulife.com.br>
2025-06-14 07:45:41 +0800

    Update authRoutes.js

--- clinica-api/backend/src/routes/authRoutes.js
+++ clinica-api/backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Rotas de autenticação
+// Rotas de autenticação de usuário
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 
